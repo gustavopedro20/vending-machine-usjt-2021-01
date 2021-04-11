0bf4f8715c8bba3170a280922f2fca50bed51b01
Gustavo Pedro <contatogustavopedro20@gmail.com>
2021-04-11 18:25:37 -0300

    Criação componente vending-machine e menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'prefix',
+    redirectTo: 'menu'
+  },
+  {
+    path: 'vending-machine',
+    loadChildren: () => import('./vending-machine/vending-machine.module').then(m => m.VendingMachineModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then(m => m.MenuModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
